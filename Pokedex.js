2fbecac60e3066c3557e3e2c3b72cc1d2492a8f5
Gustavo <163170274+yScroww@users.noreply.github.com>
2024-05-13 23:11:34 -0300

    Delete Pokedex.js
    #

--- Pokedex.js
+++
@@ -1,8 +0,0 @@
-var corpinho = document.getElementById("corpinho");
-var header = document.getElementById("header");
-document.getElementById("botao").addEventListener("click", function(){
-    corpinho.classList.toggle("corpinho");
-    corpinho.classList.toggle("corpinhopreto");
-    header.classList.toggle("headerpreto");
-    document.getElementById("nav-items").classList.toggle("nav-items-preto");
-});
