0eca390d0a7d278a99b90edd383d7c387a0cfaed
Guilherme <guittrex.2005@gmail.com>
2024-05-16 22:02:54 -0300

    v1.3
    . filtros adicionados
    . barra de pesquisa funcional
    . começo da tabela de informações

--- js/Pokedex.js
+++ js/Pokedex.js
@@ -45,4 +45,58 @@ const fetchAndDisplayPokemons = async () => {
 
 };
 
+<<<<<<< Updated upstream
 fetchAndDisplayPokemons();
+=======
+fetchAndDisplayPokemons();
+var expandir = document.querySelector("#expandir")
+var menu = document.querySelector(".menu-lateral")
+var btnExpandir = document.querySelector(".btn-expandir");
+expandir.addEventListener("click", function () {
+    menu.classList.toggle("aberto")
+    if (document.getElementById("pesquisa").style.display == "flex") {
+        document.getElementById("pesquisa").style.display = "none"
+        document.querySelector(".pokemon-list").style.opacity = "0"
+        document.querySelector(".pokeinfo").style.border = "6px solid black"
+        document.querySelector(".borda-pokeinfo").style.width = "35vw"
+        document.querySelector(".borda-pokeinfo").style.height = "70vh"
+        document.querySelector(".bolaesq").style.display = "block"
+        document.querySelector(".boladir").style.display = "block"
+    }
+    else {
+        document.getElementById("pesquisa").style.display = "flex"
+        document.querySelector(".pokemon-list").style.opacity = "1"
+        document.querySelector(".pokeinfo").style.border = "0"
+        document.querySelector(".borda-pokeinfo").style.width = "0"
+        document.querySelector(".borda-pokeinfo").style.height = "0"
+        document.querySelector(".bolaesq").style.display = "none"
+        document.querySelector(".boladir").style.display = "none"
+    }
+    btnExpandir.classList.toggle("escala-invertida");
+})
+
+var menuItem = document.querySelectorAll(".item-menu button")
+function selectLink() {
+    menuItem.forEach((item) =>
+        item.classList.remove("ativo")
+    )
+    this.classList.add("ativo")
+}
+
+menuItem.forEach((item) =>
+    item.addEventListener("click", selectLink)
+)
+
+
+document.getElementById("lupa").addEventListener("keyup", function (e) {
+    const searchData = e.target.value.toLowerCase();
+
+    const filteredPokemons = pokemons.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(searchData) || pokemon.id.toString().includes(searchData);
+    })
+    pokemonList.innerHTML = '';
+    filteredPokemons.forEach((pokemon) => {
+        displayPokemon(pokemon);
+    });
+});
+>>>>>>> Stashed changes
